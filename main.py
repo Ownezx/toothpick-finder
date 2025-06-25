0fdae6ef042ea62e46077a9d0cb7abf7951bdb93
Elliot Drees <elliot@trellick.net>
2025-06-25 19:43:53 -0400

    First baby things with with stag

---
+++ main.py
@@ -0,0 +1,30 @@
+# %%
+import stag
+import cv2
+import numpy as np
+import os
+
+workdir: str = "Images/"
+output_dir: str = "Output/"
+
+
+file_list = os.listdir(workdir)
+
+
+for file in file_list:
+
+    print(file)
+    # load image
+    image = cv2.imread(workdir + file)
+
+    # detect markers
+    (corners, ids, rejected_corners) = stag.detectMarkers(image, 19)
+
+    # draw detected markers with ids
+    stag.drawDetectedMarkers(image, corners, ids)
+
+    # draw rejected quads without ids with different color
+    stag.drawDetectedMarkers(image, rejected_corners, border_color=(255, 0, 0))
+
+    # save resulting image
+    cv2.imwrite(output_dir + file, image)
