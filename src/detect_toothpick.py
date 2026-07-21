3f0aeda7f8c20e2414e216c5cfc31165f6c0e2cb
Elliot <elliot@trellick.net>
2026-07-21 17:52:18 -0400

    First kind of okay version

--- src/detect_toothpick.py
+++ src/detect_toothpick.py
@@ -5,6 +5,10 @@ import numpy as np
 import argparse
 from pathlib import Path
 
+from numpy._typing import NDArray
+
+
+WORK_FOLDER = ""
 
 def get_arguments():
     parser = argparse.ArgumentParser(
@@ -22,10 +26,17 @@ def get_arguments():
         default="tf_detect_output",
         help="Output file (default: %(default)s)"
     )
+
     parser.add_argument(
         "-e", "--export-image",
         action="store_true",
         help="Exports images with the detected toothpick in the ouput (otherwise just saves the segment data)"
+    )
+
+    parser.add_argument(
+        "-s", "--show-images",
+        action="store_true",
+        help="Shows the images as they are generated"
     )
 
     return parser.parse_args()
@@ -34,11 +45,29 @@ def get_arguments():
 def main_cli():
     launch_arguments = get_arguments()
 
+    global WORK_FOLDER
+    print(launch_arguments.input)
+    WORK_FOLDER = launch_arguments.output
+
     if Path(launch_arguments.input).is_dir():
         raise argparse.ArgumentError("Folder are not currently supported.")
 
+    Path(WORK_FOLDER).mkdir(exist_ok=True)
+
     lines = detect_lines(launch_arguments.input)
-    show_result(launch_arguments.input, lines)
+
+    out_image = generate_result_image(launch_arguments.input, lines)
+
+    if launch_arguments.export_image:
+        image_name = Path(launch_arguments.input).name
+        print(f"Exporting image to {WORK_FOLDER}/{image_name}")
+        assert(cv2.imwrite(f"{WORK_FOLDER}/{image_name}",out_image))
+
+    if launch_arguments.show_images:
+        show_result(out_image)
+
+
+
 
 
 def detect_lines(image_path: str):
@@ -66,16 +95,28 @@ def detect_lines(image_path: str):
         maxLineGap=15        # maximum gap between line segments
     )
 
-def show_result(image_path:str, lines:MatLike):
-    loaded_image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+def generate_result_image(input:str|np.ndarray, lines:MatLike):
+    if type(input) is str :
+        loaded_image = cv2.imread(input, cv2.IMREAD_COLOR)
+    elif type(input) is NDArray:
+        loaded_image = input
+    else:
+        raise TypeError("Invalid image, needs path or ndarray")
 
     # Draw detected lines
     if lines is not None:
         for x1, y1, x2, y2 in lines[:, 0]:
             cv2.line(loaded_image, (x1, y1), (x2, y2), (0, 0, 255), 10)
+    return loaded_image
 
+def show_result(input:str|np.ndarray):
+    if type(input) is str :
+        loaded_image = cv2.imread(input, cv2.IMREAD_COLOR)
+    elif type(input) is np.ndarray:
+        loaded_image = input
+    else:
+        raise TypeError("Invalid image, needs path or ndarray")
 
     cv2.imshow("Detected Toothpicks", loaded_image)
-    cv2.imwrite("outImage.jpg",loaded_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
