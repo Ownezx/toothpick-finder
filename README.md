09de5225a2bf9c371e62bc3495dd712132f83305
Elliot <elliot@trellick.net>
2026-07-30 19:05:21 -0400

    Added default colmap image mapping

--- README.md
+++ README.md
@@ -12,13 +12,17 @@ Just a repo for a botanist to find toothpicks in grass with a precision of (hope
 ### Running from source
 
 OSX:
+
 - Install colmap `brew install colmap`
 - Install python3.14 + and install the required depencencies defined in the pyproject `pip install -e .`
-- Make the `pycolmap_regen.zsh` and `start_colmap.zsh` executable with `chmod +x`
-- Run `pycolmap_regen.zsh`  
+- Run the `tf-colmap --init` command
+- Place your desired images in the project `images` folder
+- To launch the image matching part of colmap run `tf-colmap -a MapImages`
 
 ### Fiducial marker generation
 
-- Download HD19 fiducial markers from https://github.com/ManfredStoiber/stag-python, a link is present in their readme
+- Download the depending submodule `git submodule init && git submodule update`
 - Install typst https://github.com/typst/typst
-- Run the `typst compile fid_markers.typ` command
+  - Alternatively you can just use the tinymist plugin for VSCode to handle that for you
+- Go in the `ToPrint` folder
+- Run the `typst compile AprilMarkers.typ` command
--- src/colmap.py
+++ src/colmap.py
@@ -1,4 +1,55 @@
+import argparse
 from pathlib import Path
+
+import pycolmap
+
+
+def colmap_cli() -> None:
+    """
+    Colmap project utility for the toothpicker finder project
+    """
+    parser = argparse.ArgumentParser(
+        description="Colmap project utility for the toothpicker finder project."
+    )
+
+    parser.add_argument(
+        "-p",
+        "--project-dir",
+        type=Path,
+        default=Path("colmap_project"),
+        help="Directory where the COLMAP project will be created.",
+    )
+    parser.add_argument(
+        "--init",
+        action="store_true",
+        help="Initializes a new colmap project",
+    )
+
+    # Configure the multi-choice argument
+    parser.add_argument(
+        "-a",
+        "--action",
+        choices=["MapImages"],
+        help="Action to do on the colmap project",
+    )
+
+    args = parser.parse_args()
+
+    if args.init:
+        initialize_colmap_project(args.project_dir)
+        print(
+            f"Finished initializing, you can now place your images in {args.project_dir}/images"
+        )
+        return
+
+    if not Path(args.project_dir).exists():
+        raise ValueError("Please initialize a project with 'tf-colmap --init'")
+
+    if args.action == "MapImages":
+        run_incremental_mapping(args.project_dir)
+        return
+
+    parser.print_help()
 
 
 def initialize_colmap_project(
@@ -15,10 +66,45 @@ def initialize_colmap_project(
         Path where the COLMAP database should be created.
     """
     sparse_dir = project_dir / "sparse"
+    image_path = project_dir / "images"
     database_path = project_dir / "database.db"
 
     # Create directory structure
-    project_dir.mkdir(parents=True, exist_ok=True)
-    sparse_dir.mkdir(exist_ok=True)
+    project_dir.mkdir(parents=True)
+    sparse_dir.mkdir()
+    image_path.mkdir()
+
+    project_file = Path(project_dir) / "project.ini"
+    project_file.write_text(
+        f"""
+database_path={database_path.resolve()}
+image_path={Path(image_path).resolve()}
+""",
+        encoding="utf-8",
+    )
 
     return project_dir, database_path
+
+
+def run_incremental_mapping(project_dir: Path = Path("colmap")):
+    sparse_dir = project_dir / "sparse"
+    image_path = project_dir / "images"
+    database_path = project_dir / "database.db"
+
+    if image_path.is_dir() and not any(image_path.iterdir()):
+        raise FileNotFoundError("No images were found in the image directory")
+
+    pycolmap.extract_features(
+        database_path=str(database_path),
+        image_path=str(image_path),
+    )
+
+    pycolmap.match_exhaustive(
+        database_path=str(database_path),
+    )
+
+    pycolmap.incremental_mapping(
+        database_path=str(database_path),
+        image_path=str(image_path),
+        output_path=str(sparse_dir),
+    )
